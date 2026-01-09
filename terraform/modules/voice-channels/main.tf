ec4454216575961a1cee1d3152dd3f17e393fd44
Rodrigolppz <rodrigobracens@gmail.com>
2026-01-09 20:51:46 -0300

    add voice category

--- terraform/modules/voice-channels/main.tf
+++ terraform/modules/voice-channels/main.tf
@@ -1,6 +1,14 @@
-resource discord_voice_channel general {
-  name = "Test-Voice-Channel"
+resource "discord_category_channel" "voice" {
+  name      = "Canais de Voz"
   server_id = var.server-id
+  position  = 2
+}
+
+
+resource discord_voice_channel general {
+  name = "Test-Voice-Channel4"
+  server_id = var.server-id
+  category = discord_category_channel.voice.id
   position = 0
 
   sync_perms_with_category = false
